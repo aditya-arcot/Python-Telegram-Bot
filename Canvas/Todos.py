b1e6f0e2e6b797579f38f27c9b90e56d84a4d810
adityaarcot <arcot.aditya@utexas.edu>
2022-08-25 14:43:22 -0500

    added course code to reminders

--- Canvas/Todos.py
+++ Canvas/Todos.py
@@ -21,15 +21,19 @@ def get_all_todo_attributes(key):
         if date != None:
             date = arrow.get(date, 'YYYY-MM-DDTHH:mm:ssZ').to('US/Central')
 
-        all_todo_attributes.append([data['name'], date])
+        course_id = data['course_id']
+        course = canvas.get_course(course_id)
+        course_code = course.course_code.split(' ')
+        modified_course_code = (''.join(course_code[:-1]) + ' ' + course_code[-1]).upper()
+
+        all_todo_attributes.append([data['name'], date, modified_course_code])
 
     #test_attributes = ['test', arrow.get('2022-03-30T10:00:00Z').replace(tzinfo='US/Central')]
     #all_todo_attributes.append(test_attributes)
 
     return all_todo_attributes
 
-
-#out - list of [name, due date, time diff]
+#out - list of [name, due date, course code, time diff]
 min_diff = timedelta()
 max_diff = timedelta(hours=12)
 def filter_todos(all_todo_attributes):
@@ -41,13 +45,12 @@ def filter_todos(all_todo_attributes):
             diff = i[1] - arrow.now('US/Central')
 
             if diff > min_diff and diff <= max_diff:
-                filtered_todo_attributes.append([i[0], i[1], diff])
+                filtered_todo_attributes.append([i[0], i[1], i[2], diff])
     
     return filtered_todo_attributes
 
 def sort_todo_attributes(attributes):
     return sorted(attributes, key = lambda x: x[1])
-
 
 def get_todos(key):
     all_todo_attributes = get_all_todo_attributes(key)
@@ -61,36 +64,36 @@ def get_todos(key):
         for i in all_todo_attributes:
             s = CanvasUtils.get_output_string(i[0])
             if i[1] != None:
-                out.append(' - {date} ({day}) {time} {name}'.format(date=i[1].strftime("%m/%d"), day = i[1].strftime('%a'),\
-                    time = i[1].strftime("%H:%M"), name = s))
+                out.append(' - {date} ({day}) {time} - {course} - {name}'.format(date=i[1].strftime("%m/%d"), day = i[1].strftime('%a'),\
+                    time = i[1].strftime("%H:%M"), course = i[2], name = s))
             else:
                 out.append(' - {}'.format(s))
 
         out.sort()
         out.insert(0, '{date} Upcoming Assignments ({num})'.format(date = curr.strftime("%m/%d"), num = len(all_todo_attributes)))
         
-    return out
+    return out	
 
 def get_reminders(key):
-    all_todo_attributes = get_all_todo_attributes(key) #list of [name, Arrow object]
-    filtered_todo_attributes = sort_todo_attributes(filter_todos(all_todo_attributes))
+    all_todo_attributes = get_all_todo_attributes(key) #list of [name, Arrow object, course code]
+    filtered_todo_attributes = sort_todo_attributes(filter_todos(all_todo_attributes)) #list of [name, Arrow object, course code, time diff]
 
     out = []
     for i in filtered_todo_attributes:
         name = CanvasUtils.get_output_string(i[0])
-        t = CanvasUtils.get_rounded_time_remaining(i[2])
+        
+        t = CanvasUtils.get_rounded_time_remaining(i[3])
 
         st = 'Assignment due in about {}'.format(t[1])
         if t[0] == 'm':
             st += ' minutes!! - '
         else:
             st += ' hours! - '
-        st += name
+        st += '({}) '.format(i[2]) + name
 
         out.append(st)
     
     return out
-
 
 def main(mode, key):
     if mode == 'all': #send all reminders
