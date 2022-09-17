a2037be8308d132f75b2acf150e6d1559abb05e3
adityaarcot <arcot.aditya@utexas.edu>
2022-09-16 19:56:00 -0500

    Removed sandbox argument used in testing

--- Telegram/telegram_listener.py
+++ Telegram/telegram_listener.py
@@ -33,7 +33,7 @@ import nasa
 wait_for_internet.main()
 
 telegram_ids, telegram_names = telegram_utils.get_users_info()
-token = telegram_utils.get_token(sandbox=True)
+token = telegram_utils.get_token()
 bot = Bot(token)
 
 def help_msg():
