8b09029bd6546d747daccc9bfd3921196094ac12
aditya-arcot <arcot.aditya@utexas.edu>
2023-01-02 23:28:35 -0600

    moved files to new repo

--- utils.py
+++
@@ -1,8 +0,0 @@
-'''General use utility functions'''
-
-import time
-
-def total_time(start):
-    '''Returns formatted total time given start time'''
-
-    return f'total time - {str(round(time.time() - start, 2))} s'
