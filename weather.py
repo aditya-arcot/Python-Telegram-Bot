ac032cf1c13dc7e344b9646e4999e2bc130d2122
aditya-arcot <arcot.aditya@utexas.edu>
2024-09-18 00:58:56 -0500

    open weather api change

--- weather.py
+++ weather.py
@@ -7,8 +7,8 @@ import requests
 def main(lat, lng, api_key):
     '''Formats data from request and returns'''
 
-    url = (f'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lng}'
-            f'&exclude=minutely,hourly,alerts&appid={api_key}&units=imperial')
+    url = (f'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lng}'
+        f'&exclude=minutely,hourly,alerts&appid={api_key}&units=imperial')
 
     request = requests.get(url, timeout=5).json()
 
@@ -25,13 +25,12 @@ def main(lat, lng, api_key):
 
     low = day['temp']['min']
     high = day['temp']['max']
-    desc = curr['weather'][0]['description'].capitalize()
     sunrise = datetime.strftime(datetime.fromtimestamp(day['sunrise']), '%-I:%M %p')
     sunset = datetime.strftime(datetime.fromtimestamp(day['sunset']), '%-I:%M %p')
 
     out = ['<b><u>Weather for current location:</u></b>']
+    out.append(f'- {day['summary']}')
     out.append(f'- Feels like {curr["feels_like"]} {degree_f}')
     out.append(f'- Low: {low} {degree_f}, High: {high} {degree_f}')
-    out.append(f'- Description: {desc}')
     out.append(f'- Sunrise: {sunrise}, Sunset: {sunset}')
     return out
