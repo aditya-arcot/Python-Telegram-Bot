f1dcde1ae402ef79552c70afb1228bf980288d09
aditya-arcot <arcot.aditya@utexas.edu>
2025-07-12 14:28:52 -0500

    fix run script directory issues

--- run_file.sh
+++ run_file.sh
@@ -1,7 +1,32 @@
 #!/bin/bash
 
-source .venv/bin/activate
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+VENV="$SCRIPT_DIR/.venv/bin/activate"
+if [ -f "$VENV" ]; then
+    echo "Activating virtual environment at $VENV"
+    source "$VENV"
+else
+    echo "Error: Virtual environment not found at $VENV"
+    exit 1
+fi
+
 PYTHON=$(which python3)
 echo "Using Python from: $PYTHON"
 
-$PYTHON -u "$@" 
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <script.py> [args...]"
+    exit 1
+fi
+
+SCRIPT="$SCRIPT_DIR/$1"
+shift
+if [ ! -f "$SCRIPT" ]; then
+    echo "Error: Script '$SCRIPT' not found."
+    exit 1
+fi
+
+set -euo pipefail
+
+echo "Running script: $SCRIPT"
+echo "Using arguments: $@"
+$PYTHON -u "$SCRIPT" "$@"
